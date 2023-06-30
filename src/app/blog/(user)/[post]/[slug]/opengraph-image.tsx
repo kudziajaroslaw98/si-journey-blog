f40fc5e9aaf611b2e371cd4cfc50ecf0c66b1df7
Jaroslaw Kudzia <jaroslawkudzia73@gmail.com>
2023-06-30 14:54:51 +0200

    open-graph wip

--- src/app/blog/(user)/[post]/[slug]/opengraph-image.tsx
+++ src/app/blog/(user)/[post]/[slug]/opengraph-image.tsx
@@ -1,26 +1,36 @@
 import { ImageResponse } from 'next/server';
-import Image from 'next/image';
-import urlFor from '@/lib/urlFor.ts';
 
-export const alt = 'About Acme';
+export const alt = 'Twitter Card Image';
 export const size = {
   width: 1200,
   height: 630,
 };
 export const contentType = 'image/png';
 
-export default async function OpengraphImage({
-  params,
-}: {
-  params: { slug: string };
-}) {
+export default async function Image({ params }: { params: { slug: string } }) {
   const post = await fetch(`/blog/post/${params.slug}`).then((res) =>
     res.json()
   );
-
   console.log(post);
 
   return new ImageResponse(
-    <Image src={urlFor(post.mainImage).url()} alt="open graph image" />
+    (
+      <div
+        style={{
+          fontSize: 48,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {post.title}
+      </div>
+    ),
+    {
+      ...size,
+    }
   );
 }
