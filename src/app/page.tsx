ac1ecd74d2572b44088d23b1ad65846d7abe9381
Jaroslaw Kudzia <jaroslawkudzia73@gmail.com>
2023-06-30 07:02:02 +0200

    header is now fixed, hero section image is now height screen on phone resolutions, added About Us Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import Image3 from '@/public/images/image-3.jpg';
 import Image4 from '@/public/images/image-4.jpg';
 import Image5 from '@/public/images/image-5.jpg';
 import TestimonialCardComponent from '@/components/testimonial-card.component.tsx';
+import Arrow from '@/public/images/arrow.svg';
+import jarekPhoto from '@/public/images/jarek-photo-phone2.jpg';
 
 function Page() {
   return (
@@ -26,6 +28,7 @@ function Page() {
               No ads, no distractions.
             </p>
           </div>
+
           <Link href="/blog">
             <ButtonComponent
               text="Start Reading"
@@ -36,31 +39,35 @@ function Page() {
           </Link>
         </div>
 
-        <div className="absolute sm:translate-x-[35%] right-0 top-0 w-full h-full flex flex-row-reverse gap-8 -z-10">
+        <div className="absolute sm:translate-x-[35%] right-0 top-0 w-full h-full flex flex-row-reverse gap-8 -z-20 ">
           <Image
             src={Image1}
             alt="Background tile"
-            className="background-image-common brightness-[.2] sm:brightness-[.4]"
+            className="background-image-common brightness-[.2] sm:brightness-[.4] !h-screen sm:!h-auto"
             priority
           />
+
           <Image
             src={Image2}
             alt="Background tile"
             className="background-image-common blur-[2px] brightness-[.2] xl:brightness-[.6] object-left hidden sm:block"
             priority
           />
+
           <Image
             src={Image3}
             alt="Background tile"
             className="background-image-common blur-[4px] brightness-[.2] xl:brightness-[.5] hidden sm:block"
             priority
           />
+
           <Image
             src={Image4}
             alt="Background tile"
             className="background-image-common blur-[6px] brightness-[.4] hidden md:block"
             priority
           />
+
           <Image
             src={Image5}
             alt="Background tile"
@@ -69,50 +76,59 @@ function Page() {
           />
         </div>
       </div>
+
       <div className="w-full bg-emperor-100 text-emperor-900">
         <div className="xl:max-w-6xl xl:mx-auto py-36 space-y-36 px-12 sm:px-24 xl:px-0">
           <h1 className="text-[3rem] sm:text-[3.5rem] md:text-[4rem] leading-[1.05] font-medium -tracking-[0.09rem] max-w-[35rem] text-center md:text-left">
             Stay <span className="gradient-text">curious</span> yet{' '}
             <span className="gradient-text">focused</span>
           </h1>
+
           <div className="flex justify-center space-y-16 md:space-y-0  md:justify-between flex-wrap">
             <div className="md:w-[15.875rem] m-4">
               <h3 className="flex relative text-2xl -tracking-wide font-bold leading-[1.875rem]">
                 Continuous <br /> Growth
                 <span className="circle1 absolute w-[6.6875rem] h-[6.6875rem] z-20 left-[6rem] -top-[1.9375rem] rounded-full" />
               </h3>
+
               <span className="flex mt-12 text-xl leading-[1.875rem] font-open-sans">
                 As you evolve, so does our content. Stay engaged with fresh
                 insights, ideas, and techniques to support your continuous
                 growth journey.
               </span>
             </div>
+
             <div className="md:w-[15.875rem] m-4">
               <h3 className="flex relative text-2xl -tracking-wide font-bold">
                 Mindful <br /> Consumption
                 <span className="circle2 absolute w-[6.6875rem] h-[6.6875rem] z-20 left-[6rem] -top-[1.9375rem] rounded-full" />
               </h3>
+
               <span className="flex mt-12 text-xl leading-[1.875rem] font-open-sans">
                 We promote mindful consumption of content, helping you stay
                 focused and absorb information more effectively.
               </span>
             </div>
+
             <div className="md:w-[15.875rem] m-4">
               <h3 className="flex relative text-2xl -tracking-wide font-bold">
                 Ad-Free <br /> Experience
                 <span className="circle3 absolute w-[6.6875rem] h-[6.6875rem] z-20 left-[6rem] -top-[1.9375rem] rounded-full" />
               </h3>
+
               <span className="flex mt-12 text-xl leading-[1.875rem] font-open-sans">
                 Say goodbye to disruptive ads. Our platform is
                 advertisement-free, meaning no interruptions in your
                 self-improvement journey.
               </span>
             </div>
+
             <div className="md:w-[15.875rem] m-4">
               <h3 className="flex relative text-2xl -tracking-wide font-bold">
                 Focused <br /> Content
                 <span className="circle4 absolute w-[6.6875rem] h-[6.6875rem] z-20 left-[6rem] -top-[1.9375rem] rounded-full" />
               </h3>
+
               <span className="flex mt-12 text-xl leading-[1.875rem] font-open-sans">
                 Our blog provides streamlined and targeted information, reducing
                 the amount of superfluous content you need to sift through.
@@ -121,40 +137,48 @@ function Page() {
           </div>
         </div>
       </div>
+
       <div className="w-full text-emperor-100">
         <div className="max-w-6xl xl:mx-auto h-[49.3125rem] space-y-24 relative px-12 sm:px-24 xl:px-0">
           <div className="space-y-8">
             <h2 className="text-[2rem] sm:text-[3rem] md:text-[3.5rem] leading-[1.1]  font-semibold -tracking-[0.09rem] max-w-[50rem] mx-auto text-center">
               Don’t take our word for it.
             </h2>
+
             <h1 className="text-[2rem] sm:text-[3rem] md:text-[3.5rem] leading-[1.1]  gradient-text font-extrabold -tracking-[0.09rem] max-w-[50rem] mx-auto text-center">
               Trust our readers
             </h1>
           </div>
+
           <div className="flex overflow-x-visible space-x-8 absolute -left-[13.5rem] xl:-left-[18.5rem]">
             <TestimonialCardComponent
               className="xl:-bottom-8 xl:blur-[1px]"
               name="John"
               text="This blog has genuinely changed my life. The self-improvement strategies are practical, and I can apply them to my daily life seamlessly. Can't recommend it enough!"
             />
+
             <TestimonialCardComponent
               className="xl:-bottom-4"
               name="Samantha"
               text="The blog posts on this site are incredibly insightful and inspiring. I've learned so much and have seen noticeable changes in my life since I started reading regularly"
             />
+
             <TestimonialCardComponent
               name="Peter"
               text="Every article is like a mini life lesson. It's wonderful how much knowledge and inspiration I can gather from each post. Truly an amazing blog!"
             />
+
             <TestimonialCardComponent
               name="Lisa"
               text="I always look forward to new blog posts. The content is always fresh, relevant, and full of practical tips. It's been a great tool for my personal growth journey."
             />
+
             <TestimonialCardComponent
               className="xl:-bottom-4"
               name="Alex"
               text="I love the depth of the articles, and they're always presented in such a clear and concise manner. Reading this blog has become a staple part of my morning routine."
             />
+
             <TestimonialCardComponent
               className="xl:-bottom-8 xl:blur-[1px]"
               name="Michelle"
@@ -163,12 +187,73 @@ function Page() {
           </div>
         </div>
       </div>
-      <div className="w-full text-emperor-100">
+
+      <div className="w-full bg-emperor-100 text-emperor-900">
         <div className="xl:max-w-6xl xl:mx-auto py-36 space-y-36 px-12 sm:px-24 xl:px-0">
-          <h1 className="text-5xl font-medium -tracking-[0.09rem] max-w-[35rem]">
-            Stay <span className="gradient-text">curious</span> yet{' '}
-            <span className="gradient-text">focused</span>
+          <h1 className="text-[3rem] sm:text-[3.5rem] md:text-[4rem] leading-[1.05] font-medium -tracking-[0.09rem] max-w-[35rem] text-center lg:text-left">
+            About <span className="gradient-text">Us</span>
           </h1>
+
+          <div className="flex flex-col lg:flex-row justify-center items-center">
+            <div className="w-[26.5rem] h-[25.5625rem] relative">
+              <Image
+                className="object-cover object-center shadow-xl rounded-lg"
+                src={jarekPhoto}
+                unoptimized
+                fill
+                alt="Jarek picture"
+              />
+            </div>
+
+            <div className="flex flex-col w-full h-fit pt-24 lg:pt-0 lg:pl-24 justify-between">
+              <div className="space-y-12">
+                <div className="flex flex-col text-center lg:text-left">
+                  <h2 className="font-bold text-4xl">Jarosław Kudzia</h2>
+
+                  <h5 className="font-bold text-picton-blue-600 text-xl">
+                    Founder of Journey
+                  </h5>
+                </div>
+
+                <p className="font-open-sans leading-normal">
+                  As a seasoned programmer and an avid self-improvement
+                  advocate, I blend my analytical mindset with a passion for
+                  personal growth. I believe in fostering physical prowess
+                  through calisthenics and martial arts, while also nurturing
+                  the mind and social skills.
+                  <br />
+                  <br />
+                  I strive to make the world a better place by simplifying the
+                  journey of self-improvement. I aim to make it less
+                  overwhelming for those starting out, just as I once was, by
+                  providing structured, easily digestible content.
+                  <br />
+                  <br />
+                  Embark on this journey with me, as we navigate the sea of
+                  self-improvement <b>together</b>.
+                </p>
+              </div>
+
+              <div className="flex items-center pt-12">
+                <span className="w-3 h-3 bg-emperor-200 rounded-full mr-4" />
+
+                <span className="w-3 h-3 bg-emperor-200 rounded-full mr-4" />
+
+                <span className="w-3 h-3 bg-emperor-200 rounded-full mr-4" />
+
+                <button
+                  className="ml-4 flex space-x-2 justify-center items-center"
+                  type="button"
+                  disabled
+                >
+                  <span className="text-emperor-200">More staff soon</span>
+                  <span className="scale-75 hidden">
+                    <Arrow />
+                  </span>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,29 +3,31 @@ import { NavLinkComponent } from '@/components/nav-link.component.tsx';
 
 export default function Header() {
   return (
-    <header className="xl:max-w-6xl xl:mx-auto sticky flex items-center justify-between space-x-2 px-5 py-2 md:px-10 md:py-5">
-      <div className="flex items-center gap-2 sm:gap-6">
-        <Logo className="object-fit w-8 h-8" />
-        <h1 className="text-xl md:text-3xl uppercase leading-6 text-emperor-100">
-          Journey
-        </h1>
-      </div>
-      <div className="flex justify-end items-center gap-6 md:gap-12 flex-1">
-        <NavLinkComponent
-          href="https://www.patreon.com/JourneySI"
-          label="Become Patreon"
-          button
-        />
+    <header className="fixed w-full z-50 bg-emperor-950 space-x-2 px-5 py-2 drop-shadow-2xl md:px-10 md:py-5">
+      <div className="w-full flex items-center justify-between xl:max-w-6xl xl:mx-auto">
+        <div className="flex items-center gap-2 sm:gap-6">
+          <Logo className="object-fit w-8 h-8" />
+          <h1 className="text-xl md:text-3xl uppercase leading-6 text-emperor-100">
+            Journey
+          </h1>
+        </div>
+        <div className="flex justify-end items-center gap-6 md:gap-12 flex-1">
+          <NavLinkComponent
+            href="https://www.patreon.com/JourneySI"
+            label="Become Patreon"
+            button
+          />
 
-        <NavLinkComponent
-          href="/"
-          label="Home"
-          button={false}
-          scroll={false}
-          className="hidden sm:block"
-        />
+          <NavLinkComponent
+            href="/"
+            label="Home"
+            button={false}
+            scroll={false}
+            className="hidden sm:block"
+          />
 
-        <NavLinkComponent href="/blog" label="Blog" button={false} />
+          <NavLinkComponent href="/blog" label="Blog" button={false} />
+        </div>
       </div>
     </header>
   );
