aa5c88be1c50e099c84c6dd05cb06120c1824a12
Jaroslaw Kudzia <jaroslawkudzia73@gmail.com>
2023-07-02 07:46:59 +0200

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 import dynamic from 'next/dynamic';
 import React from 'react';
@@ -8,61 +9,62 @@ const Header = dynamic(() => import('@/components/header.tsx'));
 // @ts-ignore
 dynamic(() => import('easymde/dist/easymde.min.css'));
 export const metadata: Metadata = {
-  metadataBase: new URL(
-    'https://si-journey-blog-git-develop-kj44389.vercel.app/'
-  ),
-  title: 'Journey - blog',
-  viewport: 'width=device-width, initial-scale=1.0',
-  openGraph: {
-    type: 'website',
-    url: 'https://si-journey-blog-git-develop-kj44389.vercel.app/',
-    title: 'Journey - blog',
-    description:
-      "Explore Journey's blog - your go-to guide for self-improvement and personal growth. Dive into our articles to inspire change, encourage positive habits, and lead a fulfilling life. Begin your journey towards becoming the best version of yourself today.",
-    images: [
-      {
-        url: 'https://si-journey-blog-git-develop-kj44389.vercel.app/static/images/blog-banner.webp',
-        width: 1200,
-        height: 630,
-        alt: 'Journey Blog',
-      },
-    ],
-  },
-  authors: [
-    { name: 'Jarek Kudzia', url: 'https://github.com/kudziajaroslaw98' },
-  ],
-  keywords: [
-    'self-improvement',
-    'personal growth',
-    'blog',
-    'journey',
-    'motivation',
-    'mindset',
-    'fighting addictions',
-  ],
-  creator: 'Jarek Kudzia',
-  description:
-    "Explore Journey's blog - your go-to guide for self-improvement and personal growth. Dive into our articles to inspire change, encourage positive habits, and lead a fulfilling life. Begin your journey towards becoming the best version of yourself today.",
+	metadataBase: new URL(
+		'https://si-journey-blog-git-develop-kj44389.vercel.app/'
+	),
+	title: 'Journey - blog',
+	viewport: 'width=device-width, initial-scale=1.0',
+	openGraph: {
+		type: 'website',
+		url: 'https://si-journey-blog-git-develop-kj44389.vercel.app/',
+		title: 'Journey - blog',
+		description:
+			"Explore Journey's blog - your go-to guide for self-improvement and personal growth. Dive into our articles to inspire change, encourage positive habits, and lead a fulfilling life. Begin your journey towards becoming the best version of yourself today.",
+		images: [
+			{
+				url: 'https://si-journey-blog-git-develop-kj44389.vercel.app/static/images/blog-banner.webp',
+				width: 1200,
+				height: 630,
+				alt: 'Journey Blog',
+			},
+		],
+	},
+	authors: [
+		{ name: 'Jarek Kudzia', url: 'https://github.com/kudziajaroslaw98' },
+	],
+	keywords: [
+		'self-improvement',
+		'personal growth',
+		'blog',
+		'journey',
+		'motivation',
+		'mindset',
+		'fighting addictions',
+	],
+	creator: 'Jarek Kudzia',
+	description:
+		"Explore Journey's blog - your go-to guide for self-improvement and personal growth. Dive into our articles to inspire change, encourage positive habits, and lead a fulfilling life. Begin your journey towards becoming the best version of yourself today.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className="scroll-smooth">
-      <body className="bg-emperor-950 text-emperor-100 no-scrollbar font-inter overflow-x-clip">
-        <div className="w-full">
-          <div className="">
-            <Header />
+	return (
+		<html lang='en' className='scroll-smooth'>
+			<body className='bg-emperor-950 text-emperor-100 no-scrollbar font-inter overflow-x-clip'>
+				<div className='w-full'>
+					<div className=''>
+						<Header />
 
-            <div className="pt-16 pb-16">{children}</div>
+						<div className='pt-16 pb-16'>{children}</div>
 
-            <Footer />
-          </div>
-        </div>
-      </body>
-    </html>
-  );
+						<Footer />
+					</div>
+				</div>
+				<Analytics />
+			</body>
+		</html>
+	);
 }
