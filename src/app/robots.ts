6502c8b0d7e4c484154a92f76ce970330f457a8f
Jaroslaw Kudzia <jaroslawkudzia73@gmail.com>
2023-07-31 06:34:58 +0200

    added robots.txt and sitemap.xml

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: {
 			userAgent: '*',
-			allow: ['/', '/blog/categories/all'],
+			allow: ['/', '/blog/category/all'],
 			disallow: ['/blog/studio'],
 		},
 		sitemap: `${getAbsolutePath()}/sitemap.xml`,
